70e8ba56d8c9e54c693a0285fd9a915a75781e5b
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2025-11-17 03:56:31 +0000

    refactor: remove message tunneling components
    - Remove TunnelServer.kt, TunnelClient.kt, ResponseCodes.kt, StreamUtils.kt
    - Remove StreamUtilsTest.kt
    - Remove demo-server-responses.sh

--- src/main/kotlin/tunneling/ResponseCodes.kt
+++
@@ -1,8 +0,0 @@
-package tunneling
-
-object ResponseCode {
-    const val AUTH_FAILED: Byte = 0
-    const val AUTH_SUCCESS: Byte = 1
-    const val AUTH_MALFORMED: Byte = 2
-    const val MSG_RECEIVED: Byte = 3
-}
